405ad2d435194e7e86a87533a8a96eda342b12d8
kangkang <xiyoulinux.kangyijie@gmail.com>
2017-11-06 12:19:13 +0800

    update get time function

--- getTime.cpp
+++ getTime.cpp
@@ -7,31 +7,57 @@
 
 #include <iostream>
 #include <cstring>
+#include <sys/time.h>
 using namespace std;
 static unsigned long long getCpuKhz()
 {
-        FILE *fp = fopen("/proc/cpuinfo","r");
-        if(!fp) return 1;
-        char buf[4096] = {0};
-        fread(buf,1,sizeof(buf),fp);
-        fclose(fp);
+    FILE *fp = fopen("/proc/cpuinfo","r");
+    if(!fp) 
+        return 1;
+    char buf[4096] = {0};
+    fread(buf,1,sizeof(buf),fp);
+    fclose(fp);
 
-        char *lp = strstr(buf,"cpu MHz");
-        if(!lp) return 1;
-        cout  << lp << endl;
-        lp += strlen("cpu MHz");
-        while(*lp == ' ' || *lp == '\t' || *lp == ':')
-    {
-                ++lp;
-            
-    }
+    char *lp = strstr(buf,"cpu MHz");
+    if(!lp) 
+        return 1;
+    lp += strlen("cpu MHz");
+    while(*lp == ' ' || *lp == '\t' || *lp == ':')
+        ++lp;
 
-        double mhz = atof(lp);
-        unsigned long long u = (unsigned long long)(mhz * 1000);
-        return u;
+    double mhz = atof(lp);
+    unsigned long long u = (unsigned long long)(mhz * 1000);
+    return u;
 
+}
+static unsigned long long counter(void)
+{
+    register uint32_t lo, hi;
+    register unsigned long long o;
+    __asm__ __volatile__ (
+            "rdtscp" : "=a"(lo), "=d"(hi)
+            );
+    o = hi;
+    o <<= 32;
+    return (o | lo);
+
+}
+unsigned long long getTime()
+{
+    static uint32_t khz = getCpuKhz();
+    return counter() / khz;
+}
+unsigned long long getTime2()
+{
+    struct timeval now = { 0 };
+    gettimeofday( &now,NULL );
+    unsigned long long u = now.tv_sec;
+    u *= 1000;
+    u += now.tv_usec / 1000;
+    return u;
 }
 int main()
 {
-    cout << getCpuKhz() << endl;
+    cout << getTime() << endl;
+    cout << getTime2() << endl;
 }
